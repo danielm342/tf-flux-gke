3c56c949a4f49b8cd7c560f508dca27139837d85
danielm342 <164362274+danielm342@users.noreply.github.com>
2024-05-11 14:38:44 +0300

    add main.tf

---
+++ main.tf
@@ -0,0 +1,42 @@
+module "github_repository" {
+  source                   = "github.com/den-vasyliev/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux0"
+}
+
+module "gke_cluster" {
+  source         = "github.com/den-vasyliev/tf-google-gke-cluster?ref=gke_auth"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = var.GKE_NUM_NODES
+}
+
+module "flux_bootstrap" {
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=gke_auth"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  github_token = var.GITHUB_TOKEN
+  config_host  = module.gke_cluster.config_host
+  config_token = module.gke_cluster.config_token
+  config_ca    = module.gke_cluster.config_ca
+}
+
+module "tls_private_key" {
+  source    = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
+  algorithm = var.algorithm
+}
+
+module "gke-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  use_existing_k8s_sa = true
+  name                = "kustomize-controller"
+  namespace           = "flux-system"
+  project_id          = var.GOOGLE_PROJECT
+  cluster_name        = "main"
+  location            = var.GOOGLE_REGION
+  annotate_k8s_sa     = true
+  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+}
